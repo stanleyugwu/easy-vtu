c6301157347862fdad51d7b2219c48617f171c7f
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-17 15:09:07 +0100

    feat: init app entry and install packages

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //Screens
+import SignUpScreen from './screens/SignUpScreen';
+import SplashScreen from './screens/SplashScreen/index';
 import SignInScreen from './screens/SignInScreen';
-
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+import HomeScreen from './screens/HomeScreen';
+import { useSelector } from 'react-redux';
+import QuickSubHomeScreen from './screens/QuikSubHomeScreen';
 
 //Create Navigation Stack
 const Stack = createNativeStackNavigator();
 
-const Home = (props) => (<View>
-  <Text>Am Home</Text>
-</View>)
-
 const App = () => {
+  const signedIn = useSelector(state => state.user.signedIn);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Sign-In" component={SignInScreen}/>
-        <Stack.Screen name="Home" component={Home}/>
+        {
+          !signedIn ? (
+            <>
+              <Stack.Screen name="Splash" component={SplashScreen}/>
+              <Stack.Screen name="Sign-Up" component={SignUpScreen}/>
+              <Stack.Screen name="Sign-In" component={SignInScreen}/>
+              <Stack.Screen name="Forgot-Password" component={ForgotPasswordScreen}/>
+              <Stack.Screen name="QuickSub" component={QuickSubHomeScreen}/>
+            </>
+          ) : (
+            <Stack.Screen name="Home" component={HomeScreen}/>
+          )
+        }
       </Stack.Navigator>
     </NavigationContainer>
   )
