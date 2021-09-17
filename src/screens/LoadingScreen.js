f61255cbb0d8240b7dfe9c09b1c778124a3424bd
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-17 15:11:01 +0100

    feat: switch to local lottie animation library

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, View, Text, ImageBackground } from 'react-native';
-import AnimatedLoader from 'react-native-animated-loader';
+import tw from '../lib/tailwind';
+import LottieView from 'lottie-react-native';
 
 const LoadingScreen = (props) => (
     <>
@@ -8,12 +9,14 @@ const LoadingScreen = (props) => (
             source={require('../../assets/splash.png')} 
             style={{width:'100%',height:'100%',alignItems:'center',alignContent:'flex-end',justifyContent:'flex-end',flexDirection:'column'}}
         >
-        <View style={{backgroundColor:'white'}}>
-            <AnimatedLoader
-                visible={true}
-                source={require('../../assets/loader.json')}
-            />
-        </View>
+            <View style={tw`absolute bottom-4`}>
+                <LottieView
+                    source={require('../../assets/loader.json')}
+                    style={{width:100,height:100}}
+                    autoPlay={true}
+                    speed={2.5}
+                />
+            </View>
         </ImageBackground>
     </>
 )
