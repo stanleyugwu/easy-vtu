e048951ebd5f4954364fc92d4923b9f8d38a952d
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-22 00:32:24 +0100

    feat: seperate auth logic, provider, and routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import App from './App';
-import { Nunito_400Regular, Nunito_600SemiBold } from '@expo-google-fonts/nunito';
-import { useFonts } from 'expo-font';
-import LoadingScreen from './screens/LoadingScreen';
 import {Provider} from 'react-redux';
 import store from './store';
+import AppWrapper from './app/AppWrapper';
 
 const Main = () => {
-
-    let [fontsLoaded] = useFonts({
-        'Nunito':Nunito_400Regular,
-        'Nunito-Medium':Nunito_600SemiBold,
-    });
-    if(!fontsLoaded) return <LoadingScreen/>;
-
     return (
         <Provider store={store}>
-            <App/>
+            <AppWrapper/>
         </Provider>
     )
 }
