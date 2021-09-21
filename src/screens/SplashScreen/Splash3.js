000c3d06229bef27602baac13d4538df635ae65f
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-22 00:48:36 +0100

    refactor: modify fade animation delay in splash

--- src/screens/SplashScreen/Splash3.js
+++ src/screens/SplashScreen/Splash3.js
@@ -11,33 +11,32 @@ const Splash3 = (props) => {
     const dispatch = useDispatch();
 
     return (
-        <FadeInView style={tw`p-3 mx-auto w-full`}>
+        <FadeInView style={tw`p-3 mx-auto w-full`} duration={300}>
             <Image 
                 source={require('../../../assets/splash-images/img-1.jpg')} 
                 style={{width:220,height:220,flexShrink:0,flexGrow:0,alignSelf:'center'}}
             />
             <Title style={tw`my-6 text-center`}>Start Topping Up Now!</Title>
             <View style={tw`w-full mt-6`}>
-                <FadeInView delay={200} slideUp={true}>
+                <FadeInView delay={100} slideUp={true}>
                     <CustomButton 
                         text="Create An Account" 
                         style={tw`w-full`} 
                         onPress={() => {props.navigate('Sign-Up')}}
                     />
                 </FadeInView>
-                <FadeInView delay={300} slideUp={true}>
+                <FadeInView delay={200} slideUp={true}>
                     <CustomButton 
                         text="SIGN IN" 
                         style={tw`w-full my-4`} 
                         onPress={() => {props.navigate('Sign-In')}}
                     />
                 </FadeInView>
-                <FadeInView delay={400} slideUp={true}>
+                <FadeInView delay={300} slideUp={true}>
                     <CustomButton 
                         text="Quick Sub"   
                         style={tw`w-full`}
                         onPress={() => {
-                            dispatch(useQuickSub());
                             props.navigate('QuickSub');
                         }}
                     />
