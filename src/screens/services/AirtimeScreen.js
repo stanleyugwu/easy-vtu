7fb444b45b9d8007899979f5f6985c0819b4e0d4
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-22 00:46:54 +0100

    feat: create mock services screens

---
+++ src/screens/services/AirtimeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import ScreenContainer from '../../components/CustomSafeAreaView';
+
+const AirtimeScreen  = () => {
+    return (
+        <ScreenContainer>
+            <Text>Hello Airtime</Text>
+        </ScreenContainer>
+    )
+}
+
+export default AirtimeScreen
---
+++ src/screens/services/DataScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import ScreenContainer from '../../components/CustomSafeAreaView';
+
+const DataScreen  = () => {
+    return (
+        <ScreenContainer>
+            <Text>Hello Data</Text>
+        </ScreenContainer>
+    )
+}
+
+export default DataScreen
