43faf045fdddaa80c6b27ff8ed84ea34c1abc1ac
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-24 16:43:12 +0100

    fix: move talwind config
    moved tailwind config from `theme` to `extend` cus of replacements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,26 @@ module.exports = {
     purge:['./src/**/*.{js,jsx,ts,tsx}',],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-      colors:{
-        black:'#343434',
-        white:'#fafafa',
-        gray:{
-          DEFAULT:'#888888',
-          light:'#e5e5e5'
-        }
-      },
-      fontFamily:{
-        sans:['Nunito','sans-serif'],
-        serif:['serif']
-      },
       extend: {
         colors:{
           primary:'#2d3e61',
-          accent:'#f7b902'
+          accent:'#f7b902',
+          black:'#343434',
+          white:'#fafafa',
+          gray:{
+            DEFAULT:'#888888',
+            light:'#e5e5e5'
+          }
         },
         fontFamily:{
           nunitomed:['Nunito-Medium','sans-serif'],
           nunitobold:['Nunito-Bold','sans-serif'],
+          sans:['Nunito','sans-serif'],
+          serif:['serif']
         },
         fontSize:{
-          'btn':15
+          'btn':15,
+          '1.5xl':22
         }
       },
     },
