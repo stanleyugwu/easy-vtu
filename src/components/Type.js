baedb5ac46244484dd4362c6381c854f2f05f4be
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-24 18:01:44 +0100

    feat: make `Type` default export an object of components

--- src/components/Type.js
+++ src/components/Type.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text as EText} from 'react-native';
 import tw from '../lib/tailwind';
 
-export default CustomText = ({children,style,...rest}) => {
-    return <Text style={[tw`text-center text-base text-black font-nunitomed`,style]} {...rest}>{children}</Text>
+export const Text = ({children,style,...rest}) => {
+    return <EText style={[tw`text-center text-base text-black font-nunitomed`,style]} {...rest}>{children}</EText>
 }
 
 export const Title = ({children,style,...rest}) => {
-    return <Text style={[tw`text-center text-black font-nunitobold text-1.5xl`,style]} {...rest}>{children}</Text>
+    return <EText style={[tw`text-center text-black font-nunitobold text-1.5xl`,style]} {...rest}>{children}</EText>
 }
+
+const Type = {
+    Text,
+    Title
+}
+
+export default Type
