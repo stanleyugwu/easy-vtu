46d4f270017ff96f81bcfa2b364e4ddc8ca5589d
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-25 13:52:57 +0100

    feat: make backButton component take props

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -4,8 +4,8 @@ import { Text } from 'react-native-paper';
 import {Entypo as Icon} from '@expo/vector-icons';
 import tw from '../lib/tailwind';
 
-const BackButton = () => {
-    return <TouchableOpacity style={tw`p-2 flex-row`}>
+const BackButton = ({onPress,style, ...rest}) => {
+    return <TouchableOpacity style={tw.style('p-2 flex-row',style)} onPress={onPress} {...rest}>
         <Icon name="chevron-thin-left" size={25}/>
         <Text style={tw`text-base`}>Back</Text>
     </TouchableOpacity>
