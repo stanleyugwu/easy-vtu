9c89b2221429322ddcc301d63c4e7511989718fc
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-25 13:53:50 +0100

    feat: replace lower circle button with top backButton

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -8,6 +8,7 @@ import {Text} from '../../components/Type';
 import Splash1 from './Splash1';
 import Splash2 from './Splash2';
 import Splash3 from './Splash3';
+import BackButton from '../../components/BackButton';
 
 const SplashScreen = (props) => {
     const [currentSplash,setCurrentSplash] = useState(1);
@@ -15,6 +16,11 @@ const SplashScreen = (props) => {
     return (
         <ScreenContainer containerStyle={tw`justify-around`}>
             <StatusBar backgroundColor={tw.color('primary')}/>
+            {
+                currentSplash == 3 ?
+                    <BackButton onPress={_ => setCurrentSplash(currentSplash-1)}/>
+                : null
+            }
             {
                 currentSplash == 1 ? (
                     <Splash1/>
@@ -41,21 +47,14 @@ const SplashScreen = (props) => {
                             <TouchableOpacity style={tw`ml-2 pt-2`} onPress={_ => setCurrentSplash(3)}>
                                 <Text style={tw`uppercase text-xl text-accent font-nunitobold`}>Skip</Text>
                             </TouchableOpacity>
-                        ) : (currentSplash == 3) ? (
-                            <CircleButton 
-                                icon="arrow-left" 
-                                color={tw.color('accent')} 
-                                size={25} 
-                                onPress={_ => setCurrentSplash(currentSplash-1)}
-                            />
-                        ) : (
+                        ) : (currentSplash > 1 && currentSplash < 3) ? (
                             <CircleButton 
                                 icon="arrow-left" 
                                 color={tw.color('accent')} 
                                 size={35} 
                                 onPress={_ => setCurrentSplash(currentSplash-1)}
                             />
-                        )
+                        ) : null
                     }
                     {
                         // currentSplash == 2 ? (
