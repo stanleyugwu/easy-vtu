0ad636f57a3dcac3519648d0302a43e632a2f3a0
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-09-26 19:40:02 +0100

    feat: themed forgotPasswordScreen

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -2,7 +2,8 @@ import React from 'react';
 import tw from '../lib/tailwind';
 import CustomButton from '../components/CustomButton';
 import { ImageBackground, View, Image, ScrollView  } from 'react-native';
-import { TextInput, Title, Divider, Text } from 'react-native-paper';
+import { TextInput, Divider,} from 'react-native-paper';
+import {Text, Title} from '../components/Type';
 import {AntDesign as Icon} from '@expo/vector-icons';
 
 const ForgotPasswordScreen = (props) => {
@@ -18,9 +19,9 @@ const ForgotPasswordScreen = (props) => {
                         source={require('../../assets/icon.png')}
                         style={{ width: 100, height: 100, resizeMode: 'contain', alignSelf: 'center',flexGrow:0 }}
                     />
-                    <Title style={tw`text-center`}>We All Forget, Don't Worry.</Title>
+                    <Title>We All Forget, Don't Worry.</Title>
                     <Divider style={tw`h-1 bg-primary rounded-full w-32 mx-auto mt-2`}/>
-                    <Text style={tw`text-base text-center mt-5`}>
+                    <Text style={tw`mt-5`}>
                         Just enter your e-mail address below and we'll help you get back to your account.
                     </Text>
                     <ScrollView style={tw`mt-9`}>
@@ -33,7 +34,7 @@ const ForgotPasswordScreen = (props) => {
                         <CustomButton 
                             text="Regain Access" 
                             containerProps={{style:tw`mt-7`}} 
-                            right={<Icon name="rightcircleo" size={20} color={tw.color('white')}/>}
+                            right={<Icon name="rightcircleo" size={20} color={tw.color('accent')}/>}
                         />
                     </ScrollView>
                 </View>
