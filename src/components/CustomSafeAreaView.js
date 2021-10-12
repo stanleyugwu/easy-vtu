c152fadbd569c7c5f113713fa0fdbc932818d823
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-10-12 16:55:59 +0100

    feat: add mode prop to customSafeArea

--- src/components/CustomSafeAreaView.js
+++ src/components/CustomSafeAreaView.js
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "../lib/tailwind";
 
 export default ({children,containerStyle,...rest}) => (
-    <SafeAreaView style={tw.style('bg-white h-full',{...containerStyle})} {...rest}>
+    <SafeAreaView style={tw.style('bg-white h-full',{...containerStyle})} mode="margin" {...rest}>
         {children}
     </SafeAreaView>
 )
