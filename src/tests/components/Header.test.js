ab2497557ef3b95398f428fe400dd5cac08bfaf4
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-10-14 14:55:38 +0100

    test: modify and add new tests for Header component

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -9,11 +9,36 @@ describe('Header Component', () => {
         expect(getByA11yLabel('menu-icon')).toBeTruthy();
     });
 
+    it(`given a callback via onMenuPress, calls the callback when menu icon is pressed`, () => {
+        const mockFn = jest.fn();
+        const {getByA11yLabel} = render(<Header onMenuPress={mockFn}/>);
+        const menuIcon = getByA11yLabel('menu-icon');
+        fireEvent(menuIcon,'onPress');
+        expect(mockFn).toBeCalled();
+    })
+
+    
     it('renders notification icon', () => {
         const {getByA11yLabel} = render(<Header/>);
         expect(getByA11yLabel('notification-icon')).toBeTruthy();
     });
 
+    it(`given a callback via onNotificationPress, calls the callback when bell icon is pressed`, () => {
+        const mockFn = jest.fn();
+        const {getByA11yLabel} = render(<Header onNotificationPress={mockFn}/>);
+        const bellIcon = getByA11yLabel('notification-icon');
+        fireEvent(bellIcon,'onPress');
+        expect(mockFn).toBeCalled();
+    })
+
     ProfileAvatarTest();
+    it(`given a callback via onAvatarPress, calls the callback when profile avatar is pressed`, () => {
+        const mockFn = jest.fn();
+        const {getByA11yLabel} = render(<Header onAvatarPress={mockFn}/>);
+        const avatar = getByA11yLabel('profile-icon');
+        fireEvent(avatar,'onPress');
+        expect(mockFn).toBeCalled();
+    })
+
     WalletCardTest();
 })
