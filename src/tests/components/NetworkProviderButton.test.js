89017eaf146eba2dafcb5760136e85cda6238410
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-10-28 16:19:41 +0100

    test: add snapshot test to NetworkProviderButton component

--- src/tests/components/NetworkProviderButton.test.js
+++ src/tests/components/NetworkProviderButton.test.js
@@ -1,5 +1,6 @@
-import {act, fireEvent, render} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import NetworkProviderButton from '../../components/NetworkProviderButton';
+import renderer from 'react-test-renderer';
 
 describe(
     `
@@ -8,23 +9,25 @@ describe(
     `, 
 
     () => {
-    it('given a callback function, calls callback when the card is pressed', () => {
-        const mockFn = jest.fn();
-        const {container} = render(<NetworkProviderButton onPress={mockFn}/>)
-        fireEvent(container,'onPress');
-        expect(mockFn).toBeCalled();
-    })
+        it('given a callback function, calls callback when the card is pressed', () => {
+            const mockFn = jest.fn();
+            const {container} = render(<NetworkProviderButton onPress={mockFn}/>)
+            fireEvent(container,'onPress');
+            expect(mockFn).toBeCalled();
+        })
 
-    it('given an image src of a network provider, renders the image', () => {
-        const imgSrc = require('../../../assets/service-icons/default_network.png');
-        const {getByA11yRole} = render(<NetworkProviderButton networkImageSrc={imgSrc}/>);
-        const avatar = getByA11yRole('image-button');
-        expect(avatar.props.children[1].props.source).toBe(imgSrc)
-    })
+        it('given an image src of a network provider, renders the image', () => {
+            const imgSrc = require('../../../assets/service-icons/default_network.png');
+            const {getByA11yRole} = render(<NetworkProviderButton networkImageSrc={imgSrc}/>);
+            const avatar = getByA11yRole('image-button');
+            expect(avatar.props.children[1].props.source).toBe(imgSrc)
+            expect(renderer.create(<NetworkProviderButton/>).toJSON()).toMatchSnapshot()
+        })
 
-    it('given provider name, renders a label with the name', () => {
-        const networkName = "MTN NIGERIA";
-        const {getByA11yRole} = render(<NetworkProviderButton networkName={networkName}/>)
-        expect(getByA11yRole('text').props.children).toBe(networkName);
-    })
-})
+        it('given provider name, renders a label with the name', () => {
+            const networkName = "MTN NIGERIA";
+            const {getByA11yRole} = render(<NetworkProviderButton networkName={networkName}/>)
+            expect(getByA11yRole('text').props.children).toBe(networkName);
+        })
+    }
+)
