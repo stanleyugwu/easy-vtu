bf49d56011c20d2d646e23eca04db7df06cf8575
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-11-18 17:37:38 +0100

    improvement: customized `CustomSafeAreaView` component
    1. add doc-blocks
    2. set default props
    3. add pre-defined styles
    4. wrap children in `scrollView` and support for custom styling it

--- src/components/CustomSafeAreaView.js
+++ src/components/CustomSafeAreaView.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import { SafeAreaView } from "react-native-safe-area-context";
-import tw from "../lib/tailwind";
+import PropTypes from 'prop-types';
+import { ScrollView} from 'react-native';
 
-export default ({children,containerStyle,...rest}) => (
-    <SafeAreaView style={tw.style('bg-white h-full',{...containerStyle})} mode="margin" {...rest}>
-        {children}
+/**
+ * renders its children in the safe area of the screen. It pads, adds gray background,
+ * and wraps children in `<Scrollview/>` too.
+ */
+const SafeArea = ({children, scrollViewProps, containerStyle,...rest}) => (
+    <SafeAreaView 
+        style={[
+            {backgroundColor:'#e3e3e3',height:'100%',zIndex:99, padding:10},
+            containerStyle
+        ]} 
+        mode="margin" 
+        {...rest}
+    >
+        <ScrollView {...scrollViewProps}>
+            {children}
+        </ScrollView>
     </SafeAreaView>
 )
+
+SafeArea.propTypes = {
+    /** style for `SafeAreaView` wrapper component */
+    containerStyle:PropTypes.object,
+
+    /** extra props object for <ScrollView/> wrapper component */
+    scrollViewProps: PropTypes.object
+}
+
+SafeArea.defaultProps = {
+    containerStyle:null,
+    scrollViewProps:null
+}
+
+export default SafeArea
