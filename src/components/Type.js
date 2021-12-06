10bed0e7278b12f50eed7e42d070cec311ebda0c
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-12-06 01:38:26 +0100

    improvement: refactor component
    - Define props type with `jsDoc` comments
    - add `propTypes`

--- src/components/Type.js
+++ src/components/Type.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import {Text as EText} from 'react-native';
-import tw from '../lib/tailwind';
+import React, { ReactText } from "react";
+import { Text as NativeText, TextProps, TextStyle } from "react-native";
+import tw from "../lib/tailwind";
+import PropTypes from "prop-types";
 
-const Text = ({children,style,...rest}) => {
-    return <EText style={tw.style('text-center text-base text-black font-sans',style,)} {...rest}>{children}</EText>
-}
+/**
+ * @typedef {Object} CustomTextProps
+ * @property {TextStyle} style Text component style
+ * @property {TextProps} restOfProps Other props passed `Text` props
+ * @property {ReactText} children Component children
+ */
 
-const Title = ({children,style,...rest}) => {
-    return <EText style={[tw`text-center text-black font-sans-bold text-lg`,style]} {...rest}>{children}</EText>
-}
+/**
+ * Renders text just like native `Text` component, but with pre-defined styles that match app theme
+ * @param {CustomTextProps} props
+ */
+const Text = (props) => {
+  const { children, style, ...restOfProps } = props;
+  return (
+    <NativeText
+      style={tw.style("text-center text-base text-black font-sans", style)}
+      {...restOfProps}
+    >
+      {children}
+    </NativeText>
+  );
+};
 
-export {Text as default, Title}
+Text.propTypes = {
+  style: PropTypes.object,
+  children: PropTypes.node.isRequired,
+  restOfProps: PropTypes.object,
+};
+
+/**
+ * Renders title text with pre-defined styles that match app theme
+ * @param {CustomTextProps} props
+ */
+const Title = (props) => {
+  const { children, style, ...restOfProps } = props;
+  return (
+    <NativeText
+      style={[tw`text-center text-black font-sans-bold text-lg`, style]}
+      {...restOfProps}
+    >
+      {children}
+    </NativeText>
+  );
+};
+
+Text.propTypes = {
+  style: PropTypes.object,
+  children: PropTypes.node.isRequired,
+  restOfProps: PropTypes.object,
+};
+
+export { Text as default, Title };
