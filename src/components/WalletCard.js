b455f5b2bb769097e6177463b116976ba1c560b2
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-12-06 22:57:25 +0100

    improvement: Format source. Add jsDoc comments and prop types

--- src/components/WalletCard.js
+++ src/components/WalletCard.js
@@ -1,46 +1,99 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { Image, View } from "react-native";
 import Text from "./Type";
-import tw from '../lib/tailwind';
-import {Ionicons as Icon} from '@expo/vector-icons'
-import BoxShadowView from './BoxShadowView';
+import tw from "../lib/tailwind";
+import { Ionicons as Icon } from "@expo/vector-icons";
+import BoxShadowView from "./BoxShadowView";
+import { ViewStyle } from "react-native";
+import PropTypes from "prop-types";
 
-const WalletCard = ({balance = '#0',totalCards = 0,onAddCallback = () => null}) => {
-    return (
-        <BoxShadowView 
-            containerStyle={
-                tw.style(
-                    'w-80 max-w-md mx-auto rounded-bl-md rounded-tr-md bg-white p-3',
-                    {borderTopLeftRadius:35,borderBottomRightRadius:35}
-                )
-            }
-        >
-            <View style={tw`flex-row justify-between`}>
-                <View style={tw`flex-col justify-between pl-2`}>
-                    <View style={tw`flex-row`} accessibilityRole="tab">
-                        <Icon name="wallet-sharp" color={tw.color(`gray`)} size={20}/>
-                        <Text accessibilityLabel="wallet-label" style={tw`ml-1 text-gray`}>Wallet</Text>
-                    </View>
+/**
+ * @typedef {Object} Prop `WalletCard` component props
+ * @property {string} [accessibilityLabel] Custom accessibility label for parent `View`
+ * @property {ViewStyle} [wrapperStyle] Style for parent `View`
+ * @property {number} balance Amount to display as balance
+ * @property {number} totalCards Number to display as number of saved debit card
+ * @property {function} onAddCallback Function to invoke when add button is pressed
+ */
 
-                    <Text accessibilityLabel="wallet-balance" style={tw`text-3xl ml-3 my-2`}>{balance}</Text>
-                    
-                    <View style={tw`flex-row`}>
-                        <Icon name="card-outline" size={20} color={tw.color('black')}/>
-                        <Text accessibilityLabel="cards-added" style={tw`ml-1 items-center`}>{totalCards} Card(s)</Text>
-                    </View>
-                </View>
-                <Icon 
-                    name="add-circle" 
-                    style={tw`p-0 m-0 self-center`} 
-                    accessibilityRole="button" 
-                    ellipsizeMode="middle" 
-                    size={75} 
-                    color={tw.color('primary')} 
-                    onPress={onAddCallback}
-                />
-            </View>
-        </BoxShadowView>
-    )
-}
+/**
+ * Renders a ui card having user's wallet information
+ * @param {Prop} props
+ */
+const WalletCard = React.forwardRef((/** @type {Prop} */ props, ref) => {
+  return (
+    <BoxShadowView
+      containerStyle={tw.style(
+        "w-80 max-w-md mx-auto rounded-3xl bg-white p-3",
+        props.wrapperStyle
+      )}
+    >
+      <View
+        style={tw`flex-row justify-between`}
+        accessibilityLabel={props.accessibilityLabel}
+        ref={ref}
+      >
+        <View style={tw`flex-col justify-between pl-2`}>
+          <View style={tw`flex-row`} accessibilityRole="tab">
+            <Image
+              source={require("../../assets/wallet_img.png")}
+              style={tw`w-5 h-5`}
+            />
+            <Text
+              accessibilityLabel="wallet-label"
+              style={tw`ml-1 text-gray text-sm`}
+            >
+              Wallet
+            </Text>
+          </View>
 
-export default WalletCard
+          <Text
+            accessibilityLabel="wallet-balance"
+            style={tw`text-3xl ml-4 my-2`}
+          >
+            {"\u20A6"}
+            {props.balance}
+          </Text>
+
+          <View style={tw`flex-row`}>
+            <Image
+              source={require("../../assets/card_img.png")}
+              style={tw`w-5 h-5`}
+            />
+            <Text
+              accessibilityLabel="cards-added"
+              style={tw`ml-1 items-center text-sm font-nunitobold`}
+            >
+              {props.totalCards + " Card(s)"}
+            </Text>
+          </View>
+        </View>
+        <Icon
+          name="add-circle"
+          style={tw`p-0 m-0 self-center`}
+          accessibilityRole="button"
+          ellipsizeMode="middle"
+          size={75}
+          color={tw.color("primary")}
+          onPress={props.onAddCallback}
+        />
+      </View>
+    </BoxShadowView>
+  );
+});
+
+WalletCard.propTypes = {
+  accessibilityLabel: PropTypes.string,
+  wrapperStyle: PropTypes.object,
+  balance: PropTypes.number.isRequired,
+  totalCards: PropTypes.number.isRequired,
+  onAddCallback: PropTypes.func.isRequired,
+};
+
+WalletCard.defaultProps = {
+  accessibilityLabel: "wallet card",
+  balance: 0,
+  totalCards: 0,
+};
+
+export default React.memo(WalletCard);
