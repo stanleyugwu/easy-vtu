a9b5846870ef45873d1c6c8944425d8ccb982e1c
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-12-06 22:54:53 +0100

    improvement: format source with prettier and declare navigation object inside component as opposed to being passed as prop

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,41 +1,74 @@
-import React from 'react';
-import ServiceCard from './ServiceCard';
-import tw from '../lib/tailwind';
-import { ScrollView } from 'react-native-gesture-handler';
+import React from "react";
+import ServiceCard from "./ServiceCard";
+import tw from "../lib/tailwind";
+import { ScrollView } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/core";
 
-const Services = ({navigation = {navigate:() => null}}) => {
-    return (
-        <ScrollView contentContainerStyle={tw`flex-row flex-wrap justify-around p-4`}>
-            <ServiceCard 
-                title="Buy Airtime"
-                source={require('../../assets/service-icons/airtime.png')}
-                onPress={_ => navigation.navigate('Airtime')}
-            />
-            <ServiceCard 
-                title="Buy Data" 
-                source={require('../../assets/service-icons/data.png')}
-                onPress={_ => navigation.navigate('Data')}
-            />
-            <ServiceCard 
-                title="Buy Electricity" 
-                source={require('../../assets/service-icons/electricity.png')}
-                onPress={_ => navigation.navigate('Electricity')}
-            />
-            <ServiceCard 
-                title="Tv Cable" 
-                source={require('../../assets/service-icons/cable.png')}
-                onPress={_ => navigation.navigate('Cable')}
-            />
-            <ServiceCard 
-                title="Scratch Card" 
-                source={require('../../assets/service-icons/card.png')}
-                onPress={_ => navigation.navigate('ScratchCard')}
-            />
-            <ServiceCard
-                source={require('../../assets/service-icons/cable.png')}
-            />
-        </ScrollView>
-    )
-}
+/**
+ * Renders app services in cards with `ServiceCard` component
+ */
+const Services = () => {
+  const navigation = useNavigation();
 
-export default Services
+  return (
+    <ScrollView
+      contentContainerStyle={tw`flex-row flex-wrap justify-around content-around p-4`}
+    >
+      <ServiceCard
+        animationDelay={300}
+        bounciness={10}
+        title="Buy Airtime"
+        iconSrc={require("../../assets/service_icons/airtime.png")}
+        onPress={(_) =>
+          navigation.navigate("NetworkProviders", {
+            headerTitle: "Airtime Top-Up",
+            serviceType: "Airtime",
+          })
+        }
+      />
+      <ServiceCard
+        animationDelay={400}
+        bounciness={10}
+        title="Buy Data"
+        iconSrc={require("../../assets/service_icons/data.png")}
+        onPress={(_) =>
+          navigation.navigate("NetworkProviders", {
+            headerTitle: "Data Top-Up",
+            serviceType: "Data",
+          })
+        }
+      />
+      <ServiceCard
+        animationDelay={500}
+        bounciness={10}
+        title="Buy Electricity"
+        containerStyle={tw`my-3`}
+        iconSrc={require("../../assets/service_icons/electricity.png")}
+        onPress={(_) => navigation.navigate("NetworkProviders")}
+      />
+
+      <ServiceCard
+        animationDelay={600}
+        bounciness={10}
+        title="Tv Cable"
+        containerStyle={tw`my-3`}
+        iconSrc={require("../../assets/service_icons/cable.png")}
+        onPress={(_) => navigation.navigate("NetworkProviders")}
+      />
+      <ServiceCard
+        animationDelay={700}
+        bounciness={10}
+        title="Scratch Card"
+        iconSrc={require("../../assets/service_icons/card.png")}
+        onPress={(_) => navigation.navigate("NetworkProviders")}
+      />
+      <ServiceCard
+        animationDelay={800}
+        bounciness={10}
+        iconSrc={require("../../assets/service_icons/more.png")}
+      />
+    </ScrollView>
+  );
+};
+
+export default React.memo(Services);
