6cf8ee955657b0a2a830c38657a5bd32b7c495a1
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-12-06 17:59:21 +0100

    improvement: modify` ImageButton` component
    - Document with `jsDoc` comment
    - Format with prettier
    - Swap `TouchableRipple` wrapper button to `TouchableOpacity`

--- src/components/ImageButton.js
+++ src/components/ImageButton.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { TouchableRipple } from "react-native-paper";
 import Text from "./Type";
 import { Ionicons as Icon } from "@expo/vector-icons";
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, ViewStyle, View, ColorValue, ImageSourcePropType, GestureResponderEvent } from "react-native";
 import tw from "../lib/tailwind";
 import PropTypes from "prop-types";
 import defaultImg from "../../assets/image_placeholder.png";
@@ -10,23 +9,33 @@ import BoxShadowView from "./BoxShadowView";
 import PressResizerView from "./PressResizerView";
 
 /**
- * renders a pressable ui card having an image to the left, a text label,
- *
- * and an icon to the right.
+ * @typedef {Object} ImageButtonProps
+ * @property {string} label Button label text
+ * @property {ColorValue} labelColor Color for text label
+ * @property {ImageSourcePropType} imgSrc The image source (either a remote URL or a local file resource).\
+ * This prop can also contain several remote URLs, specified together with their width and height and potentially with scale/other URI arguments. The native side will then choose the best uri to display based on the measured size of the image container. A cache property can be added to control how networked request interacts with the local cache.
+ * The currently supported formats are png, jpg, jpeg, bmp, gif, webp (Android only), psd (iOS only).
+ * @property {string} rightIconName Name of icon to render to the right of button
+ * @property {(event: GestureResponderEvent) => void} onPress Callback to be called when button is pressed
+ * @property {ViewStyle} containerStyle Styles for `View` parent container
+ */
+
+/**
+ * Renders a pressable ui card having an image to the left, a text label, and an icon to the right.
+ * @param {ImageButtonProps} props
  */
 const ImageButton = (props) => {
   const resolvedSrc =
-    typeof props.imgSrc === "number" 
-    ? props.imgSrc 
-    : { uri: props.imgSrc };
+    typeof props.imgSrc === "number" ? props.imgSrc : { uri: props.imgSrc };
 
   return (
     <PressResizerView>
-      <BoxShadowView containerStyle={tw.style('rounded-md mt-8 max-w-md')}>
-        <TouchableRipple 
-          onPress={props.onPress} 
-          rippleColor={props.labelColor} 
-          borderless={true} 
+      <BoxShadowView containerStyle={tw.style("rounded-md mt-5 max-w-md")}>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={props.onPress}
+          rippleColor={props.labelColor}
+          borderless={true}
           accessibilityRole="button"
         >
           <View
@@ -48,8 +57,14 @@ const ImageButton = (props) => {
             </View>
 
             {/* label wrapper */}
-            <View style={tw`flex-row items-center justify-end w-9/12 px-6`} accessibilityLabel={"label text wrapper"}>
-              <Text style={tw.style("pr-6", { color: props.labelColor })} accessibilityLabel="label text">
+            <View
+              style={tw`flex-row items-center justify-end w-9/12 px-6`}
+              accessibilityLabel={"label text wrapper"}
+            >
+              <Text
+                style={tw.style("pr-10", { color: props.labelColor })}
+                accessibilityLabel="label text"
+              >
                 {props.label}
               </Text>
               <Icon
@@ -61,34 +76,18 @@ const ImageButton = (props) => {
               />
             </View>
           </View>
-        </TouchableRipple>
+        </TouchableOpacity>
       </BoxShadowView>
     </PressResizerView>
   );
 };
 
 ImageButton.propTypes = {
-  /** card label text */
   label: PropTypes.string.isRequired,
-
-  /** color for text label */
   labelColor: PropTypes.string,
-
-  /** source for image rendered at the left of card.
-   *
-   * it can be a resource ID (i.e return value of `require`) for local image,
-   *
-   * a local path to image, or an http address string for online image.
-   */
   imgSrc: PropTypes.oneOfType([PropTypes.number.isRequired, PropTypes.string]),
-
-  /** name of icon to render to the right of card */
   rightIconName: PropTypes.string.isRequired,
-
-  /** callback called when card is pressed */
   onPress: PropTypes.func.isRequired,
-
-  /** styles for `View` parent container */
   containerStyle: PropTypes.object,
 };
 
@@ -96,10 +95,9 @@ ImageButton.displayName = "ImageButton";
 
 ImageButton.defaultProps = {
   label: "EasyVtu",
-  labelColor: tw.color("accent"),
+  labelColor: tw.color("light"),
   imgSrc: defaultImg,
   rightIconName: "chevron-forward-sharp",
-  containerStyle: null,
 };
 
 export default React.memo(ImageButton);
