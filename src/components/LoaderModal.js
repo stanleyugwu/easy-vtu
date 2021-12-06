144b7b7be26469890d80ace4ae78236d449e8c41
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-12-06 21:38:25 +0100

    feat: create `<LoaderModal/>` component
    create a loader component for loading indication with an optional text.
    The loader will overlay other views and show an animation on mount

---
+++ src/components/LoaderModal.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Modal, View, ViewStyle } from "react-native";
+import Text from "./Type";
+import LottieView from "lottie-react-native";
+import tw from "../lib/tailwind";
+import PropTypes from "prop-types";
+
+/**
+ * @typedef {Object} LoaderModalProps
+ * @property {string} [accessibilityLabel] Custom accessibilty label for `<Modal/>` wrapper
+ * @property {string} loadingText Optional text to show below loading animation
+ * @property {ViewStyle} [modalStyle] `View` Style for main (white colored) modal component
+ * @property {ViewStyle} [modalWrapperStyle] Style for modal wrapper component. This is the transparent area
+ */
+
+/**
+ * Renders a loading modal with animation and text
+ */
+const LoaderModal = (
+  /** @type {LoaderModalProps} */ {
+    loadingText,
+    accessibilityLabel,
+    modalWrapperStyle,
+    modalStyle,
+  }
+) => {
+  return (
+    <Modal
+      visible={true}
+      transparent={true}
+      accessibilityLabel={accessibilityLabel}
+      accessibilityRole="progressbar"
+      accessibilityState={{ busy: true, disabled: true, expanded: true }}
+    >
+      <View
+        style={[
+          { width: "100%", height: "100%", backgroundColor: "#0009" },
+          modalWrapperStyle,
+        ]}
+        accessibilityLabel="loader modal wrapper"
+      >
+        <View
+          accessibilityLabel="loader modal main"
+          style={tw.style(
+            "rounded-lg mx-auto my-auto w-64 max-w-md p-3 bg-white",
+            modalStyle
+          )}
+        >
+          <LottieView
+            source={require("../../assets/loader.json")}
+            style={{ width: 100, height: 50, alignSelf: "center" }}
+            autoPlay={true}
+            speed={2.5}
+          />
+          {loadingText ? (
+            <Text
+              accessibilityLabel="loader modal text"
+              style={tw`font-sans-bold`}
+            >
+              {loadingText}
+            </Text>
+          ) : null}
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+LoaderModal.propTypes = {
+  accessibilityLabel: PropTypes.string,
+  modalWrapperStyle: PropTypes.object,
+  modalStyle: PropTypes.object,
+  loadingText: PropTypes.string,
+};
+
+LoaderModal.defaultProps = {
+  accessibilityLabel: "Loader Dialog",
+};
+
+export default React.memo(LoaderModal);
