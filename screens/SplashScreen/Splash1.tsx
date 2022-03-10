83456d97dd131f4bf9cbb42cd3961165e1cee96f
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-03-10 14:30:25 +0100

    improvement: migrate Splash1 screen to typescript

--- screens/SplashScreen/Splash1.tsx
+++ screens/SplashScreen/Splash1.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Image } from "react-native";
-import Text, { Title } from "../../components/Type";
-import tw from "../../lib/tailwind";
+import { Title } from "../../components/Type";
 import FadeInView from "../../components/FadeInView";
 import splashImg from "../../assets/images/splash-images/img-1.jpg";
+import Text from "../../components/Themed";
+import tw from "../../lib/tailwind";
 
 const Splash1 = () => (
   <FadeInView containerStyle={tw`mx-auto items-center p-3`}>
@@ -11,8 +12,8 @@ const Splash1 = () => (
       source={splashImg}
       style={{ width: 220, height: 220, flexShrink: 0, flexGrow: 0 }}
     />
-    <Title style={tw`mt-5 text-accent`}>Buy with ease</Title>
-    <Text style={tw`text-light mt-1`}>
+    <Text type="title" style={tw`mt-5`}>Buy With Ease</Text>
+    <Text style={tw`mt-1`}>
       Very long text Very long text Very long text Very long text Very long text
       Very long text Very long text
     </Text>
