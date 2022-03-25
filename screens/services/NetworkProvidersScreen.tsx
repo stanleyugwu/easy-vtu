ff61a478ee17b71ce91cad6f48fff84479c2f384
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-03-25 13:21:11 +0100

    improvement: modify `Services` and `NetworkProviderScreen` to read remote-config values

--- screens/services/NetworkProvidersScreen.tsx
+++ screens/services/NetworkProvidersScreen.tsx
@@ -14,13 +14,22 @@ import tw from "../../lib/tailwind";
 import FlashView from "../../components/FlashView";
 import { useSelector } from "react-redux";
 import RecipientTypeModal from "../../components/RecipientTypeModal";
-import type { RootStackScreenProps, RootStackParamList } from "../../types";
 import withTile from "../../hooks/withTile";
+import remoteConfig from "@react-native-firebase/remote-config";
 
-export type NetworkProvidersNames = "Mtn" | "Glo" | "Airtel" | "9Mobile" | "Ntel" | "Smile";
-type ProvidersPrefixes = Record<NetworkProvidersNames, Array<string>>
+import type { RootStackScreenProps, RootStackParamList } from "../../types";
+import type { RemoteConfig } from "../../types";
 
-const providersPrefixes:ProvidersPrefixes = {
+export type NetworkProvidersNames =
+  | "Mtn"
+  | "Glo"
+  | "Airtel"
+  | "9Mobile"
+  | "Ntel"
+  | "Smile";
+type ProvidersPrefixes = Record<NetworkProvidersNames, Array<string>>;
+
+const providersPrefixes: ProvidersPrefixes = {
   Mtn: ["0803", "0806", "0703", "0706", "0813", "0816", "0810", "0814", "0903"],
   Glo: ["0805", "0807", "0705", "0815", "0811", "0905"],
   Airtel: ["0802", "0808", "0708", "0812", "0701", "0902"],
@@ -40,7 +49,10 @@ const providersPrefixes:ProvidersPrefixes = {
  * the label text which `imageButton` component will display is constructed from this prop e.g `'Mtn'+ 'Airtime'`,
  * and the screen name each `imageButton` will navigate to is also constructed from this property e.g `navigate('Airtime'+'Screen')`
  */
-const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"NetworkProviders">) => {
+const NetworkProvidersScreen = ({
+  route,
+  navigation,
+}: RootStackScreenProps<"NetworkProviders">) => {
   //destructure params passed to screen
   const { serviceType = "Airtime" } = route.params;
 
@@ -53,7 +65,12 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
   const titleBackhalf = serviceType + " Top-Up"; //dynamic, constructed route headerTitle back-half string
 
   //this will be populated with user's network name if gotten
-  let usersProvider:string;
+  let usersProvider: string;
+
+  // remote-config values for active network provider
+  const networkProviders = JSON.parse(
+    remoteConfig().getValue("supportedAirtimeNetworkProviders").asString()
+  ) as RemoteConfig.SupportedAirtimeNetworkProviders;
 
   const handleRecipientTypeSelect = React.useCallback((type) => {
     if (type === "Others") setRcptModalVisible(false);
@@ -75,7 +92,7 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
         headerTitle: usersProvider + " " + titleBackhalf,
         networkName: usersProvider,
         providerLogoSrc: logoSrc,
-        recipientType: 'My Self'
+        recipientType: "My Self",
       });
     } else {
       console.error(
@@ -83,7 +100,7 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
       );
       setRcptModalVisible(false);
     }
-  },[]);
+  }, []);
 
   const detectUserCarrier = React.useCallback(() => {
     let phone = "" + phoneNumber;
@@ -91,7 +108,7 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
     if (!isSignedIn) return;
 
     let phoneWithoutDialCode = phone.slice(4);
-    let prefix:string;
+    let prefix: string;
     if (phoneWithoutDialCode.startsWith("0")) {
       //handle case where number has `0` prefix
       prefix = phoneWithoutDialCode.slice(0, 4);
@@ -101,27 +118,33 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
     }
 
     for (let provider in providersPrefixes) {
-      let prefixes = providersPrefixes[provider as keyof ProvidersPrefixes]
+      let prefixes = providersPrefixes[provider as keyof ProvidersPrefixes];
       if (prefixes.indexOf(prefix) > -1) {
         usersProvider = provider;
         break;
       }
     }
-  },[]);
+  }, []);
 
   const goBackHome = React.useCallback(() => {
     navigation.goBack();
-  },[]);
+  }, []);
 
   React.useEffect(() => {
     detectUserCarrier();
   }, []);
 
   return (
-      <SafeArea style={tw`h-full`}>
-        {(!rcptModalVisible || !isSignedIn) ? (
-          <View style={tw`bg-transparent`}>
-            <Text type="title" style={tw`my-6 text-center text-on-background mx-5`}>Which network are you topping up?</Text>
+    <SafeArea style={tw`h-full`}>
+      {!rcptModalVisible || !isSignedIn ? (
+        <View style={tw`bg-transparent`}>
+          <Text
+            type="title"
+            style={tw`my-6 text-center text-on-background mx-5`}
+          >
+            Which network are you topping up?
+          </Text>
+          {networkProviders.includes("mtn") ? (
             <FlashView delay={250} bounciness={5}>
               <ImageButton
                 label={"Mtn " + serviceType}
@@ -131,11 +154,14 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
                     headerTitle: "Mtn " + titleBackhalf,
                     networkName: "Mtn",
                     providerLogoSrc: mtnLogo,
-                    recipientType:'Others'
+                    recipientType: "Others",
                   })
                 }
               />
             </FlashView>
+          ) : null}
+
+          {networkProviders.includes("airtel") ? (
             <FlashView delay={350} bounciness={5}>
               <ImageButton
                 label={"Airtel " + serviceType}
@@ -145,11 +171,14 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
                     headerTitle: "Airtel " + titleBackhalf,
                     networkName: "Airtel",
                     providerLogoSrc: airtelLogo,
-                    recipientType:'Others'
+                    recipientType: "Others",
                   })
                 }
               />
             </FlashView>
+          ) : null}
+
+          {networkProviders.includes("9mobile") ? (
             <FlashView delay={450} bounciness={5}>
               <ImageButton
                 label={"9Mobile " + serviceType}
@@ -159,11 +188,14 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
                     headerTitle: "9Mobile " + titleBackhalf,
                     networkName: "9Mobile",
                     providerLogoSrc: etisalatLogo,
-                    recipientType:'Others'
+                    recipientType: "Others",
                   })
                 }
               />
             </FlashView>
+          ) : null}
+
+          {networkProviders.includes("glo") ? (
             <FlashView delay={550} bounciness={5}>
               <ImageButton
                 label={"Glo " + serviceType}
@@ -173,24 +205,25 @@ const NetworkProvidersScreen = ({route, navigation}:RootStackScreenProps<"Networ
                     headerTitle: "Glo " + titleBackhalf,
                     networkName: "Glo",
                     providerLogoSrc: gloLogo,
-                    recipientType:'Others'
+                    recipientType: "Others",
                   })
                 }
               />
             </FlashView>
-          </View>
-        ) : null}
-        {isSignedIn && rcptModalVisible ? (
-          <RecipientTypeModal
-            onSelect={handleRecipientTypeSelect}
-            customTitle={`Who Are You Buying ${serviceType} For?`}
-            overlayColor="#000c"
-            activeIndex={0}
-            onRequestClose={goBackHome}
-            onBackgroundTouch={goBackHome}
-          />
-        ) : null}
-      </SafeArea>
+          ) : null}
+        </View>
+      ) : null}
+      {isSignedIn && rcptModalVisible ? (
+        <RecipientTypeModal
+          onSelect={handleRecipientTypeSelect}
+          customTitle={`Who Are You Buying ${serviceType} For?`}
+          overlayColor="#000c"
+          activeIndex={0}
+          onRequestClose={goBackHome}
+          onBackgroundTouch={goBackHome}
+        />
+      ) : null}
+    </SafeArea>
   );
 };
 export default React.memo(withTile(NetworkProvidersScreen, 2));
