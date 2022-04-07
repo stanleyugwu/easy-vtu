4e1e5f238fafe6832a4cffc351176ccc63fc45a1
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-04-07 18:24:29 +0100

    improvement: modify sign-in screen authentication to be functional, perform authentication with server, and show errors via snackbar

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import tw from "../lib/tailwind";
 import CurvedButton from "../components/Button";
-import { Image } from "react-native";
-import { Divider } from "react-native-paper";
+import { Dimensions, Image } from "react-native";
+import { Divider, Snackbar } from "react-native-paper";
 import InputField from "../components/InputField";
 import SafeArea from "../components/CustomSafeAreaView";
 import Text, { View } from "../components/Themed";
 import { Ionicons as Icon } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
 import useFormField from "../hooks/useFormField.hook";
-import { signIn as setSignIn } from "../store/slices/userSlice";
+import { signIn as signInAction } from "../store/slices/userSlice";
 import LoaderModal from "../components/LoaderModal";
-import axios from "axios";
-import { signIn } from "../adapters/auth.adapter";
-import { RootStackScreenProps } from "../types";
+import { RootStackScreenProps, Server } from "../types";
 import withTile from "../hooks/withTile";
+import myAxios from "../adapters/instance";
+import { AxiosResponse } from "axios";
 
 //Images
 const appIcon = require("../assets/images/icon.png");
@@ -38,15 +38,22 @@ const passwordValidator = (password: string) => {
   return true;
 };
 
+const screenHeight = Dimensions.get("window").height;
+
 /**
  * App sign in screen
+ * It's worth noting that we show validation errors inline to the input fields they occured and we show
+ * server or request error via a snackbar
  */
 const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
   const dispatch = useDispatch();
   var isMounted = true; //app mount tracker
 
-  //axios cancelToken source
-  const source = axios.CancelToken.source();
+  // This will hold the error for requests and determine if snackbar is shown
+  const [requestError, setRequestError] = React.useState("");
+
+  //axios abort controller source
+  let controller = new AbortController();
 
   const [emailValue, emailError, handleEmailChange] = useFormField({
     validator: emailValidator,
@@ -65,7 +72,7 @@ const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
   //cancel request and reset mount variable to avoid mem-leak
   navigation.addListener("beforeRemove", () => {
     isMounted = false;
-    source.cancel("Sign-In Cancelled!");
+    controller.abort();
   });
 
   const goToSignUp = React.useCallback(() => {
@@ -78,24 +85,7 @@ const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
 
   //Sign in
   const _handleSignIn = () => {
-    const profile = {
-      id: 123,
-      username: "devvie",
-      email: "stanley@gmail.com",
-      image: "https://via.placeholder.com/600/92c952",
-      isAdmin: false,
-      referrals: 0,
-      phone: "+2348066413705",
-      password: "1234567",
-      createdAt: "Thu, 07 Oct 2021 17:28:14 GMT",
-    };
-
-    //set user's profile and initiate sign in
-    dispatch(setSignIn(profile));
-    return;
-    // dispatch(setSignIn(profile))
-    // return
-
+    setRequestError(""); //hide snackbar error
     //assert empty values
     if (emailValue === "" || passwordValue === "") {
       handleEmailChange(""); //manually initiate error
@@ -109,39 +99,59 @@ const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
     //no errors
     setSigningIn(true);
 
-    // signIn(source.token,loginCreds.email,loginCreds.password)
-    // .then(response => {
-    //     console.log(response)
-    //     /**
-    //      * sets user access_token.
-    //      * setting token is not handled by signIn reducer because there might
-    //      * be a need to set token without signing in i.e setting profile, e.g when user is using
-    //      * a service as a guest
-    //      */
-    //     dispatch(setToken(response.data.access_token));
-    //     let data = response.data.data;
-    //     //set user's wallet balance
-    //     dispatch(addMoney(parseInt(data.wallet_balance,10)));
-    //     //construct profile object based on store keys
-    //     const profile = {
-    //         id:data.id,
-    //         username : data.username,
-    //         email: data.email,
-    //         image:data.image,
-    //         isAdmin:data.isAdmin,
-    //         referrals:data.no_of_referrals,
-    //         phone: data.phone,
-    //         password: loginCreds.password,
-    //         createdAt:data.created_at
-    //     }
-    //     //set user's profile and initiate sign in
-    //     dispatch(setSignIn(profile))
-    // })
-    // .catch(error => {
-    //     console.log(error)
-    //     isMounted && setError(error.message);
-    //     isMounted && setSigningIn(false);
-    // })
+    // regenerate new abortcontroller every time a request is about to be made
+    // so as to avoid permanently cancelling a request
+    controller = new AbortController();
+
+    myAxios
+      .post<any, AxiosResponse<Server.LoginResponse>, any>(
+        "/api/login",
+        {
+          email: emailValue,
+          password: passwordValue,
+        },
+        {
+          signal: controller.signal,
+        }
+      )
+      .then((response) => {
+        // @ts-ignore
+        const accessToken = response.data.access_token,
+          {
+            created_at,
+            email,
+            email_verified_at,
+            phone,
+            unique_id,
+            updated_at,
+            username,
+          } = response.data.data;
+
+          // Sign user in
+        dispatch(
+          signInAction({
+            accessToken,
+            createdAt: created_at,
+            email,
+            emailVerifiedAt: email_verified_at,
+            id: unique_id,
+            phone,
+            image:"",
+            isAdmin:false,
+            isVerified:false,
+            noOfReferrals:0,
+            referredBy:"",
+            updatedAt:updated_at,
+            username
+          })
+        );
+        
+        // isMounted && setSigningIn(false);
+      })
+      .catch((error: Server.RequestError) => {
+        setRequestError(error.message);
+        isMounted && setSigningIn(false);
+      });
   };
 
   //eye slash icon
@@ -158,7 +168,7 @@ const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
   );
 
   return (
-    <SafeArea style={tw`p-0 h-full`}>
+    <SafeArea style={{ height: screenHeight, padding: 0 }}>
       {signingIn ? <LoaderModal loadingText="Signing In" /> : null}
       <View style={tw`mx-auto w-full h-full px-4 mt-5 bg-transparent`}>
         <Image
@@ -223,7 +233,6 @@ const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
           label="Sign In"
           leftIconName="enter-outline"
           onPress={_handleSignIn}
-          style={tw`bg-primary`}
         />
         <Text style={tw`mt-4 text-center`}>
           Not a member yet?{" "}
@@ -235,6 +244,14 @@ const SignInScreen = ({ navigation }: RootStackScreenProps<"Sign-In">) => {
           </Text>
         </Text>
       </View>
+      {/* Below snack will be used to show errors */}
+      <Snackbar
+        visible={!!requestError}
+        style={tw`bg-error`}
+        children={requestError}
+        action={{ label: "OKAY", color: tw.color("on-dark") }}
+        onDismiss={() => setRequestError("")}
+      />
     </SafeArea>
   );
 };
