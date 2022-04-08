3143cf768e9aeeb864d88a36542b61517b53df10
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-04-08 10:27:44 +0100

    improvement: delete `useAuthenticate` hook because authentication has been integrated via `redux-persist` state persistence

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import useCachedResources from "./hooks/useCachedResources";
-import useAuthenticate from "./hooks/useAuthenticate";
 import useSetupRemoteConfig from "./hooks/useSetupRemoteConfig";
 import Navigation from "./navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -9,10 +8,9 @@ import tw from "./lib/tailwind";
 
 const App = () => {
   const fontsLoaded = useCachedResources();
-  const userAuthenticating = useAuthenticate();
   const settingUpRemoteConfig = useSetupRemoteConfig();
 
-  if (!fontsLoaded || userAuthenticating || settingUpRemoteConfig) {
+  if (!fontsLoaded || settingUpRemoteConfig) {
     return null;
   }
 
