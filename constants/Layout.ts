5bf980c6a700803fee168ebca2b573a7ded9262f
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-04-15 19:41:59 +0100

    improvement: add support for `screen` dimensions in `Layout` utility

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -1,12 +1,19 @@
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+const swidth = Dimensions.get("screen").width;
+const sheight = Dimensions.get("screen").height;
 
 export default {
   window: {
     width,
     height,
   },
+  screen: {
+    width:swidth,
+    height:sheight,
+  },
   isSmallDevice: width < 375,
 };
+
