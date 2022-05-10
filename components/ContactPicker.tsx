ece92fdb9f06ba747d2286223ef3b6ac15cf8f9d
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-05-10 16:32:21 +0100

    improvement: modify `<ContactPicker/>` component

--- components/ContactPicker.tsx
+++ components/ContactPicker.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import {
   ActivityIndicator,
-  GestureResponderEvent,
+  Linking,
   ListRenderItemInfo,
   TextInput,
 } from "react-native";
 import * as Contacts from "expo-contacts";
-import ModalWrapper from "./ModalWrapper";
 import { FlatList } from "react-native-gesture-handler";
 import tw from "../lib/tailwind";
 import Text, { View, ViewProps } from "./Themed";
 import debounce from "../utils/debounce";
 import Contact from "./Contact";
 import type { Contact as ContactType } from "../types";
+import Modal from "react-native-modal";
+import Layout from "../constants/Layout";
+import { Ionicons as Icon } from "@expo/vector-icons";
+import Button from "./Button";
 
 export type ContactPickerProps = {
   /** Function to be called when a contact is selected.\
    * This function will be passed an object describing the selected contact. This object has `name` and `phoneNumber` keys
    */
   onContactSelect: (selectedContact: ContactType) => void;
-  /** The onRequestClose prop allows passing a function that will be called
-   *  once the modal has been dismissed. On the Android platform, this is a required function.
+  /**
+   * Function to be called when Android back button is pressed
    */
-  onRequestClose?: () => void;
+  onBackButtonPress?: () => void;
   /** Function to call when the area around the modal is touched */
-  onBackgroundTouch?: (event: GestureResponderEvent) => void;
+  onBackdropTouch?: () => void;
+  isVisible: boolean;
 } & ViewProps;
 
 /**
@@ -49,24 +53,8 @@ const filterContacts = (contacts: Contacts.Contact[]) => {
     });
 };
 
-/**
- * Gets user's contacts
- * @returns array of users contacts
- */
-const getUserContacts = async () => {
-  const { status } = await Contacts.requestPermissionsAsync();
-  if (status === "granted") {
-    let { data } = await Contacts.getContactsAsync({
-      fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers],
-    });
-
-    return filterContacts(data);
-  }
-  return []; //there's no check for error so lets return an empty array
-};
-
 const noContacts = (
-  <Text type="title" style={tw`self-center my-auto text-gray`}>
+  <Text type="body" style={tw`self-center my-auto text-gray`}>
     No Contact Found
   </Text>
 );
@@ -85,25 +73,43 @@ const ContactPicker = React.forwardRef(
   (
     {
       onContactSelect,
-      onBackgroundTouch,
-      onRequestClose,
+      onBackdropTouch,
+      onBackButtonPress,
+      isVisible = true,
       style,
       ...otherProps
     }: ContactPickerProps,
     ref: React.ForwardedRef<any>
   ) => {
     const [loadingContacts, setContactsLoading] = React.useState(true);
+    const [deniedContactsPermission, setDeniedContactsPermission] =
+      React.useState(false);
     const [contacts, setContacts] = React.useState<ContactType[]>([]);
     const completeContacts = React.useRef<ContactType[]>();
 
-    const loadContacts = async () => {
-      let contacts = await getUserContacts();
-      setContactsLoading(false);
-      setContacts(contacts);
-      completeContacts.current = contacts;
-    };
+    /**
+     * Loads and shows user's contacts
+     * @returns array of users contacts
+     */
+    const loadUserContacts = React.useCallback(async () => {
+      const { status, granted, canAskAgain } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+        setDeniedContactsPermission(false);
+        let { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers],
+          sort: Contacts.SortTypes.FirstName,
+        });
+        setContactsLoading(false);
+        setContacts(filterContacts(data));
+        completeContacts.current = contacts;
+        return;
+      } else if(!granted || !canAskAgain){
+        setDeniedContactsPermission(true);
+      }
+    },[]);
+
     React.useEffect(() => {
-      loadContacts();
+      loadUserContacts();
     }, []);
 
     const handleTextInputChange = React.useCallback(
@@ -121,6 +127,11 @@ const ContactPicker = React.forwardRef(
       []
     );
 
+    const handleGrantContactPermission = async () => {
+      await Linking.openSettings();
+      (onBackdropTouch || onBackButtonPress)?.();
+    };
+
     // takes item and passes it to `renderContact` along with callback for when contact is selected
     const handleRenderContact = ({ item }: ListRenderItemInfo<ContactType>) => (
       <Contact
@@ -133,37 +144,76 @@ const ContactPicker = React.forwardRef(
     const searchBar = React.useMemo(() => {
       return (
         <View
-          style={tw`w-full mb-3 pt-3 rounded-t-lg items-center justify-center bg-background`}
+          style={tw`w-full flex-row mb-3 pt-3 px-2 rounded-t-2xl items-center justify-between bg-background`}
         >
           <TextInput
             placeholder="Search Contact"
-            style={tw`mx-auto py-3 pl-4 rounded-lg w-11/12 max-w-md bg-white`}
+            style={[
+              tw`py-3 pl-4 rounded-lg max-w-md bg-white`,
+              { width: "90%" },
+            ]}
             onChangeText={handleTextInputChange}
+          />
+          <Icon
+            onPress={onBackdropTouch}
+            name="chevron-down-circle"
+            size={28}
+            color={tw.color("primary")}
+            style={{ alignSelf: "flex-end", width: "10%", margin: 10 }}
           />
         </View>
       );
     }, []);
 
     return (
-      <ModalWrapper
-        visible={true}
-        modalPosition="bottom"
-        onBackgroundTouch={onBackgroundTouch}
-        onRequestClose={onRequestClose}
-        style={tw`p-0`}
+      <Modal
+        isVisible={isVisible}
+        onBackButtonPress={onBackButtonPress}
+        onBackdropPress={onBackdropTouch}
+        animationInTiming={500}
+        animationOutTiming={500}
+        deviceHeight={Layout.screen.height}
+        deviceWidth={Layout.screen.width}
+        backdropTransitionInTiming={800}
+        supportedOrientations={["portrait", "landscape"]}
+        backdropTransitionOutTiming={500}
+        animationIn={"slideInUp"}
+        animationOut={"slideOutDown"}
+        style={{
+          justifyContent: "flex-end",
+          margin: 0,
+          alignSelf: "center",
+          width: "100%",
+        }}
       >
         <View
           style={[
             tw`rounded-t-2xl`,
             {
-              height: "90%",
+              height: "70%",
+              width: "100%",
             },
             style,
           ]}
           {...otherProps}
         >
-          {loadingContacts ? (
+          {loadingContacts && !deniedContactsPermission ? (
             loadingView
+          ) : deniedContactsPermission ? (
+            <View
+              style={tw`p-4 justify-center items-center self-center m-auto`}
+            >
+              <Icon name="sad-outline" size={70} />
+              <Text style={tw`text-center mb-6`}>
+                You denied the app permission to read your contacts. You won't
+                be able to choose from your contacts for topping-up if you don't
+                grant the permission.
+              </Text>
+              <Button
+                label="Grant Permission"
+                onPress={handleGrantContactPermission}
+              />
+            </View>
           ) : (
             <FlatList
               data={contacts}
@@ -177,7 +227,7 @@ const ContactPicker = React.forwardRef(
             />
           )}
         </View>
-      </ModalWrapper>
+      </Modal>
     );
   }
 );
