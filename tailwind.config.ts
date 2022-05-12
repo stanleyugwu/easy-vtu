b0f33b7b0d5f73ff9ed42555c5cfe05ade1cfae4
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-05-12 19:04:47 +0100

    feat: change theme background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ import { TailwindConfig, TailwindThemeFontSizes } from "tailwindcss/tailwind-con
   "secondary-dark": "#F66900",
   "on-secondary": "#ffffff",
 
-  background: "#F5F5F5",
+  background: "#F8F8F8",
   "on-background": "#444444",
 
   surface: "#ffffff",
