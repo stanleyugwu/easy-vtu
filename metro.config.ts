49c3fc28d36f8e4926477f6047d637246bd73786
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-05-20 19:24:09 +0100

    improvement: add support for rendering svg images

--- metro.config.ts
+++ metro.config.ts
@@ -2,10 +2,24 @@ var getBlacklistRE = function getBlacklistRE(){
 	return new RegExp("(.*\\android\\.*|\.git|.*\\__fixtures__\\.*|node_modules[\\\\]react[\\\\]dist[\\\\].*|website\\node_modules\\.*|heapCapture\\bundle\.js|.*\\__tests__\\.*|.*\\tests\\.*)$");
 }
 
-const config =  {
-	resolver: {
-		"blacklistRE": getBlacklistRE(),
-	}
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const {getDefaultConfig} = require('expo/metro-config');
 
-module.exports = config
+module.exports = (() => { 
+    const {  
+        resolver: { 
+            sourceExts, 
+            assetExts 
+        }  
+    } = getDefaultConfig(__dirname); 
+
+    return {
+        transformer: {      
+            babelTransformerPath: require.resolve("react-native-svg-transformer")    
+        },    
+        resolver: {
+			"blacklistRE": getBlacklistRE(),
+            assetExts: assetExts.filter((ext:any) => ext !== "svg"),
+            sourceExts: [...sourceExts, "svg"]    
+        }};
+})();
