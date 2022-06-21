69cd57afa5ebd989c68dfc99b880d8f145d62807
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-06-21 17:18:43 +0100

    feat: add color prop support for coloring error text

--- components/ErrorText.tsx
+++ components/ErrorText.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import tw from "../lib/tailwind";
 import Text from "./Themed";
 export type ErrorTextProps = {
+  /** Error text. This determines whether the component will render or ot */
   error: string | undefined;
+  /** Color of the error text */
+  color?: string;
 };
 
 /**
  * Dispays error messages in style
  */
-const ErrorText = ({ error }: ErrorTextProps) =>
+const ErrorText = ({ error, color = tw.color("red-700") }: ErrorTextProps) =>
   typeof error != "string" || !error.trim() ? null : (
     <Text
-      style={tw.style(`text-red-700 pl-1 text-left font-sans-semibold`, {
+      style={tw.style(`pl-1 text-left font-sans-semibold`, {
         fontSize: 13,
+        color,
       })}
     >
       &gt; {error}
