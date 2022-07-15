159fb92eb1772c24cdadb5384a490e5619343d32
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-07-15 14:58:41 +0100

    fix: replace profile info display fields with simpler views

--- screens/protected-screens/ProfileScreen.tsx
+++ screens/protected-screens/ProfileScreen.tsx
@@ -8,7 +8,6 @@ import {
   TouchableOpacity,
 } from "react-native";
 import Text, { View } from "../../components/Themed";
-import CustomSafeAreaView from "../../components/CustomSafeAreaView";
 import tw from "../../lib/tailwind";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Layout from "../../constants/Layout";
@@ -16,7 +15,6 @@ import { ScrollView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import {
   FormFieldsTypes,
-  RootStackScreenProps,
   RootState,
   RootTabScreenProps,
   Server,
@@ -33,14 +31,31 @@ import * as FileSystem from "expo-file-system";
 import myAxios from "../../adapters/instance";
 import { Snackbar } from "react-native-paper";
 import { AxiosResponse } from "axios";
-import { signOut, updateProfile } from "../../store/slices/userSlice";
+import { updateProfile } from "../../store/slices/userSlice";
 import store from "../../store";
+import appStyles from "../../lib/appStyles";
+import RippleButton from "../../components/RippleButton";
 
 if (Platform.OS === "android") {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
     UIManager.setLayoutAnimationEnabledExperimental(true);
   }
 }
+
+export type Info = {
+  title: string;
+  label: string;
+};
+export const Info = ({ label, title }: Info) => {
+  return (
+    <View style={tw.style(`bg-surface p-2 border-b`, { borderColor: "#eee" })}>
+      <Text type="body2" style={tw`text-gray mb-2`}>
+        {title}
+      </Text>
+      <Text>{label}</Text>
+    </View>
+  );
+};
 
 const EditProfileSchema = Yup.object().shape({
   username: Yup.string()
@@ -72,7 +87,7 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
   // Snackbar triggers
   const [profileUpdateSuccessMsg, setProfileUpdateSuccessMsg] =
     React.useState<string>();
-  const [profileUpdatError, setProfileUpdatError] = React.useState<string>();
+  const [profileUpdatError, setProfileUpdateError] = React.useState<string>();
 
   if (!profile) return null;
 
@@ -96,7 +111,7 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
       return;
     }
 
-    setProfileUpdatError(undefined); // clear any error
+    setProfileUpdateError(undefined); // clear any error
 
     // Let's handle profile pic upload if user changed profile pic
     // we intentionally await for pic upload before profile details update
@@ -122,7 +137,7 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
         // TODO: Update local image url with newly returned image url from server
         // TODO: clear uploadedImage state, since image state has been updated
       } catch (error) {
-        setProfileUpdatError("Profile picture update failed!");
+        setProfileUpdateError("Profile picture update failed!");
       }
     }
 
@@ -172,7 +187,7 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
       })
       .catch((error: Server.ErrorResponse) => {
         setUpdatingProfile(undefined);
-        setProfileUpdatError(
+        setProfileUpdateError(
           "Profile info update failed. Make sure you're connected to internet and try again"
         );
       });
@@ -231,6 +246,7 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
         keyboardShouldPersistTaps="always"
       >
         <StatusBar backgroundColor={tw.color("background")} style="dark" />
+
         <View
           style={{
             flexDirection: "row",
@@ -255,13 +271,9 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
           {!editingProfile ? (
             <TouchableOpacity
               activeOpacity={0.7}
-              style={tw`bg-primary flex-row p-1 px-2 rounded-md items-center`}
+              style={tw`bg-primary flex-row p-1.5 px-2 rounded-md items-center`}
               onPress={handleStartEditing}
             >
-              <Icon
-                name="pencil-sharp"
-                style={tw`text-secondary items-center justify-center text-lg`}
-              />
               <Text type="subTitle" style={tw`text-secondary `}>
                 Edit profile
               </Text>
@@ -269,187 +281,190 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
           ) : null}
         </View>
         <View
-          style={{
-            alignSelf: "center",
-            width: 120,
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 5,
-            height: 120,
-            borderWidth: 1,
-            borderColor: tw.color("blue-100"),
-            borderRadius: 999,
-          }}
+          style={tw.style(
+            `bg-surface rounded-t-2xl pt-3 mt-4`,
+            appStyles.boxShadowSmall
+          )}
         >
-          <Image
-            source={{
-              uri:
-                editingProfile && uploadedImage
-                  ? uploadedImage
-                  : //TODO: change url to profile image url
-                    "https://cdn.dribbble.com/users/5205506/screenshots/11539440/media/430094ba50287040188fb9f581a2c8de.png?compress=1&resize=800x600",
-              height: "100%",
-              width: "100%",
-            }}
+          <View
             style={{
+              alignSelf: "center",
+              width: 120,
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 5,
+              height: 120,
+              borderWidth: 1,
+              borderColor: tw.color("blue-100"),
               borderRadius: 999,
-              backgroundColor: tw.color("blue-100"),
-              width: "100%",
-              height: "100%",
             }}
-          />
-          <Icon
-            name={"camera-outline"}
-            onPress={handleChangeProfilePic}
-            style={tw`absolute bottom-1 right-1 bg-secondary rounded-full p-1.5`}
-            size={22}
-          />
-        </View>
-
-        {editingProfile ? (
-          <>
-            <SmartTextInput
-              fieldLabel={"Username"}
-              value={formik.values.username}
-              style={tw`mt-8`}
-              onChangeText={formik.handleChange("username")}
-              fieldType="input"
-              placeholder="Enter Username"
-              error={!!formik.errors.username}
-              leftInputIcon="person-outline"
+          >
+            <Image
+              source={{
+                uri:
+                  editingProfile && uploadedImage
+                    ? uploadedImage
+                    : //TODO: change url to profile image url
+                      "https://cdn.dribbble.com/users/5205506/screenshots/11539440/media/430094ba50287040188fb9f581a2c8de.png?compress=1&resize=800x600",
+                height: "100%",
+                width: "100%",
+              }}
+              style={{
+                borderRadius: 999,
+                backgroundColor: tw.color("blue-100"),
+                width: "100%",
+                height: "100%",
+              }}
             />
-            <SmartTextInput
-              fieldLabel={"Email Address"}
-              value={formik.values.email}
-              fieldType="input"
-              extraInputProps={{ keyboardType: "email-address" }}
-              placeholder="Enter Email Address"
-              onChangeText={formik.handleChange("email")}
-              leftInputIcon="mail-outline"
-              error={!!formik.errors.email}
-            />
-            <SmartTextInput
-              fieldLabel={"Mobile Number"}
-              value={formik.values.phone}
-              placeholder="Enter Mobile Number"
-              extraInputProps={{ keyboardType: "number-pad" }}
-              onChangeText={formik.handleChange("phone")}
-              fieldType="input"
-              leftInputIcon="call-outline"
-              error={!!formik.errors.phone}
-            />
-            {formik.errors.email ||
-            formik.errors.phone ||
-            formik.errors.username ? (
-              <Text type="subTitle2" style={tw`text-error text-center mt-4`}>
-                {formik.errors.username ||
-                  formik.errors.email ||
-                  formik.errors.phone}
-              </Text>
-            ) : null}
-            <Button
-              label={"Save Changes"}
-              style={tw`mt-6`}
-              onPress={formik.handleSubmit}
-            />
-            <Text
-              type="subTitle"
-              style={tw`self-center underline text-primary mt-4`}
-              onPress={handleCancelEditing}
-            >
-              Cancel Editing
-            </Text>
-          </>
-        ) : (
-          <View style={tw`bg-transparent`}>
-            <Text type="title" style={tw`text-center`}>
-              {profile.username}
-            </Text>
-            {profile.isAdmin || profile.isVerified ? (
-              <View style={tw`justify-center items-center flex-row`}>
-                {profile.isVerified ? (
-                  <Text
-                    type="subTitle2"
-                    style={tw`bg-green-400 text-xs mr-1 p-0.5 px-2 text-background rounded-2xl`}
-                  >
-                    Verified
-                  </Text>
-                ) : null}
-                {profile.isAdmin ? (
-                  <Text
-                    type="subTitle2"
-                    style={tw`ml-1 p-0.5 px-2 text-xs bg-blue-400 text-background rounded-2xl`}
-                  >
-                    Admin
-                  </Text>
-                ) : null}
-              </View>
-            ) : null}
-            <Text style={tw`text-center`}>{profile.email}</Text>
-            <Text
-              type="subTitle"
-              style={tw`text-center mb-4 text-on-background`}
-            >
-              {profile.phone}
-            </Text>
-            <SmartTextInput
-              disabled
-              fieldLabel={"Referred By"}
-              value={profile.referredBy || "None"}
-              fieldType="input"
-              leftInputIcon="link-outline"
-            />
-            <SmartTextInput
-              disabled
-              fieldLabel={"No Of Referrals"}
-              value={profile.noOfReferrals.toString()}
-              fieldType="input"
-              leftInputIcon="person-outline"
-            />
-            <SmartTextInput
-              disabled
-              fieldLabel={"Registeration Date"}
-              value={
-                profile.createdAt
-                  ? new Date(profile.createdAt).toDateString()
-                  : "Null"
-              }
-              fieldType="input"
-              leftInputIcon="time-outline"
-            />
-            <SmartTextInput
-              disabled
-              fieldLabel="Password"
-              extraInputProps={{ secureTextEntry: true }}
-              value="incorrectkey"
-              leftInputIcon="key-outline"
-              rightInputIcon="pencil"
-              onRightInputIconPress={handleChangePassword}
-            />
-            <SmartTextInput
-              disabled
-              fieldLabel={"Last Account Update"}
-              value={
-                profile.updatedAt
-                  ? new Date(profile.updatedAt).toDateString()
-                  : "Null"
-              }
-              fieldType="input"
-              leftInputIcon="timer-outline"
-            />
-            <SmartTextInput
-              disabled
-              fieldLabel={"Email Verification Date"}
-              value={
-                profile.emailVerifiedAt
-                  ? new Date(profile.emailVerifiedAt).toDateString()
-                  : "Null"
-              }
-              fieldType="input"
-              leftInputIcon="ios-time-outline"
+            <Icon
+              name={"camera-outline"}
+              onPress={handleChangeProfilePic}
+              style={tw`absolute bottom-1 right-1 bg-secondary rounded-full p-1.5`}
+              size={22}
             />
           </View>
-        )}
+          {editingProfile ? (
+            <View style={tw`p-2 bg-surface`}>
+              <SmartTextInput
+                fieldLabel={"Username"}
+                value={formik.values.username}
+                style={tw`mt-6`}
+                onChangeText={formik.handleChange("username")}
+                fieldType="input"
+                placeholder="Enter Username"
+                error={!!formik.errors.username}
+                leftInputIcon="person-outline"
+              />
+              <SmartTextInput
+                fieldLabel={"Email Address"}
+                value={formik.values.email}
+                fieldType="input"
+                extraInputProps={{ keyboardType: "email-address" }}
+                placeholder="Enter Email Address"
+                onChangeText={formik.handleChange("email")}
+                leftInputIcon="mail-outline"
+                error={!!formik.errors.email}
+              />
+              <SmartTextInput
+                fieldLabel={"Mobile Number"}
+                value={formik.values.phone}
+                placeholder="Enter Mobile Number"
+                extraInputProps={{ keyboardType: "number-pad" }}
+                onChangeText={formik.handleChange("phone")}
+                fieldType="input"
+                leftInputIcon="call-outline"
+                error={!!formik.errors.phone}
+              />
+              {formik.errors.email ||
+              formik.errors.phone ||
+              formik.errors.username ? (
+                <Text type="subTitle2" style={tw`text-error text-center mt-4`}>
+                  {formik.errors.username ||
+                    formik.errors.email ||
+                    formik.errors.phone}
+                </Text>
+              ) : null}
+              <Button
+                label={"Save Changes"}
+                style={tw`mt-6`}
+                onPress={formik.handleSubmit}
+              />
+              <Text
+                type="subTitle"
+                style={tw`self-center underline text-primary mt-4`}
+                onPress={handleCancelEditing}
+              >
+                Cancel Editing
+              </Text>
+            </View>
+          ) : (
+            <View style={tw`bg-transparent`}>
+              <Text type="title" style={tw`text-center`}>
+                {profile.username}
+              </Text>
+              {profile.isAdmin || profile.isVerified ? (
+                <View
+                  style={tw`justify-center items-center flex-row bg-surface`}
+                >
+                  {profile.isVerified ? (
+                    <Text
+                      type="subTitle2"
+                      style={tw`bg-green-400 text-xs mr-1 p-0.5 px-2 text-background rounded-2xl`}
+                    >
+                      Verified
+                    </Text>
+                  ) : null}
+                  {profile.isAdmin ? (
+                    <Text
+                      type="subTitle2"
+                      style={tw`ml-1 p-0.5 px-2 text-xs bg-blue-400 text-background rounded-2xl`}
+                    >
+                      Admin
+                    </Text>
+                  ) : null}
+                </View>
+              ) : null}
+              <Text style={tw`text-center`}>{profile.email}</Text>
+              <Text
+                type="subTitle"
+                style={tw`text-center mb-4 text-on-background`}
+              >
+                {profile.phone}
+              </Text>
+              <View style={tw`bg-surface p-1.5 border-t border-background`}>
+                <Info
+                  label={profile.referredBy || "None"}
+                  title="Referred By"
+                />
+                <Info
+                  label={profile.noOfReferrals.toString()}
+                  title="No Of Referrals"
+                />
+                {profile.createdAt ? (
+                  <Info
+                    label={new Date(profile.createdAt).toDateString()}
+                    title="Registeration Date"
+                  />
+                ) : null}
+                <View
+                  style={tw.style(
+                    `flex-row justify-between items-center bg-transparent border-b`,
+                    { borderColor: "#eee" }
+                  )}
+                >
+                  <Info label={"************"} title="Password" />
+                  <RippleButton
+                    style={tw`bg-primary mr-4 rounded-lg justify-center`}
+                    onPress={handleChangePassword}
+                    rippleColor={tw.color("secondary")}
+                  >
+                    <View style={tw`p-1`}>
+                      <Text
+                        type="button"
+                        onPress={() => navigation.navigate("Forgot-Password")}
+                        style={tw`text-secondary-dark`}
+                      >
+                        CHANGE
+                      </Text>
+                    </View>
+                  </RippleButton>
+                </View>
+                {profile.updatedAt ? (
+                  <Info
+                    label={new Date(profile.updatedAt).toDateString()}
+                    title="Last Account Update"
+                  />
+                ) : null}
+                {profile.emailVerifiedAt ? (
+                  <Info
+                    label={new Date(profile.emailVerifiedAt).toDateString()}
+                    title="Email Verification Date"
+                  />
+                ) : null}
+              </View>
+            </View>
+          )}
+        </View>
 
         {!!updatingProfile ? (
           <LoaderModal loadingText={updatingProfile} />
@@ -467,9 +482,9 @@ const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
 
       <Snackbar
         duration={2000}
-        style={tw`bg-error`}
+        style={tw`bg-black`}
         visible={!!profileUpdatError}
-        onDismiss={() => setProfileUpdatError(undefined)}
+        onDismiss={() => setProfileUpdateError(undefined)}
       >
         <Icon name="cloud-offline-outline" size={23} /> {profileUpdatError}
       </Snackbar>
